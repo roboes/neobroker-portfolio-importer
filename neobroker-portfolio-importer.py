5355de84346445b6e5d936c7fa44a06b32a5bf30
roboes <rg.roberto@outlook.com>
2023-09-27 18:43:49 +0200

    Update

--- neobroker-portfolio-importer.py
+++ neobroker-portfolio-importer.py
@@ -19,8 +19,6 @@ from io import StringIO
 import re
 import time
 
-# import lxml
-# import openpyxl
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
---
+++ requirements-packages.py
@@ -0,0 +1,11 @@
+## Requirements Packages
+# Last update: 2023-09-27
+
+
+"""Add not imported (but required) Python packages to this file so that pipreqs adds them as a dependency to the "requirements.txt" file."""
+
+import lxml
+import openpyxl
+
+lxml.__version__
+openpyxl.__version__
