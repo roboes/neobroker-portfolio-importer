42351e2e638fa5e4d3fd23185ce66d0cee3b48a4
roboes <44812635+roboes@users.noreply.github.com>
2024-02-07 20:09:20 +0100

    Update

--- neobroker-portfolio-importer.py
+++ neobroker-portfolio-importer.py
@@ -25,8 +25,14 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
-# Set working directory
+# Settings
+
+## Set working directory
 # os.chdir(path=os.path.join(os.path.expanduser('~'), 'Downloads'))
+
+## Copy-on-Write (will be enabled by default in version 3.0)
+if pd.__version__ >= '1.5.0' and pd.__version__ < '3.0.0':
+    pd.options.mode.copy_on_write = True
 
 
 ###########
