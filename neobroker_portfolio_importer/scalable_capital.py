285b38dd89f9691b95791b11d47b5333730f427f
roboes <44812635+roboes@users.noreply.github.com>
2026-01-20 22:03:02 +0100

    New Scalable Capital fixes

--- neobroker_portfolio_importer/scalable_capital.py
+++ neobroker_portfolio_importer/scalable_capital.py
@@ -6,7 +6,7 @@ import re
 import time
 
 import pandas as pd
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -56,11 +56,19 @@ def scalable_capital_portfolio_import(
     else:
         while True:
             try:
-                WebDriverWait(driver=driver, timeout=1).until(EC.url_contains('cockpit'))
+                # "Welcome to the new Scalable" page
+                if 'auth/custodian-switch/successful-migration' in driver.current_url:
+                    try:
+                        driver.find_element(by=By.CSS_SELECTOR, value='[data-testid="custodian_switch_successful_migration_cta"]').click()
+
+                    except NoSuchElementException:
+                        pass
+
+                WebDriverWait(driver=driver, timeout=2).until(EC.url_contains('cockpit'))
                 break
 
-            except NoSuchElementException:
-                time.sleep(2)
+            except TimeoutException:
+                time.sleep(1)
 
     # Cookies: Only essentials
     time.sleep(3)
@@ -96,100 +104,95 @@ def scalable_capital_portfolio_import(
     # Create empty DataFrame
     assets_df = pd.DataFrame(data=None, index=None, dtype='str')
 
-    for broker in driver.find_elements(by=By.XPATH, value="//*[text()='Broker' or text()='Baader Broker']"):
-        broker.click()
+    # Trading venues closed
+    try:
+        driver.find_element(by=By.XPATH, value='.//button[contains(text(), "Close")]').click()
 
-        time.sleep(2)
+    except Exception:
+        pass
 
-        # Trading venues closed
-        try:
-            driver.find_element(by=By.XPATH, value='.//button[contains(text(), "Close")]').click()
+    # Portfolio id
+    portfolio_id = re.sub(pattern=r'^.*portfolioId=([^&]+).*$', repl=r'\1', string=driver.current_url, flags=0)
 
-        except Exception:
-            pass
+    portfolio_section = driver.find_element(by=By.XPATH, value="//h2[text()='Portfolio']/..")
 
-        # Portfolio id
-        portfolio_id = re.sub(pattern=r'^.*portfolioId=([^&]+).*$', repl=r'\1', string=driver.current_url, flags=0)
+    if 'Popular savings plans' in portfolio_section.text:
+        pass
 
-        portfolio_section = driver.find_element(by=By.XPATH, value="//h2[text()='Portfolio']/..")
+    else:
+        # Get only security lists for "Portfolio"
+        time.sleep(3)
 
-        if 'Popular savings plans' in portfolio_section.text:
-            continue
+        # Get 'asset_names', 'current_values' and 'isin_codes'
+        elements = driver.find_element(by=By.XPATH, value='//div[@aria-label="Portfolio"]//div')
+        elements = elements.find_elements(by=By.TAG_NAME, value='li')
 
-        else:
-            # Get only security lists for "Portfolio"
-            time.sleep(3)
+        ## Create empty lists
+        asset_names = []
+        current_values = []
+        isin_codes = []
 
-            # Get 'asset_names', 'current_values' and 'isin_codes'
-            elements = driver.find_element(by=By.XPATH, value='//div[@aria-label="Portfolio"]//div')
-            elements = elements.find_elements(by=By.TAG_NAME, value='li')
+        for element in elements:
+            asset_names.append(element.find_element(by=By.CSS_SELECTOR, value='div[data-testid="text"]').text.strip())
+            current_values.append(element.find_element(by=By.CSS_SELECTOR, value='div[aria-label="Total value"] span').text.strip())
+            isin_codes.append(element.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href'))
 
-            ## Create empty lists
-            asset_names = []
-            current_values = []
-            isin_codes = []
+        ## Delete objects
+        del element, elements
 
-            for element in elements:
-                asset_names.append(element.find_element(by=By.CSS_SELECTOR, value='div[data-testid="text"]').text.strip())
-                current_values.append(element.find_element(by=By.CSS_SELECTOR, value='div[aria-label="Total value"] span').text.strip())
-                isin_codes.append(element.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href'))
+        ## Clean 'isin_codes'
+        isin_codes = [re.sub(pattern=r'https://de.scalable.capital/broker/security\?isin=|&portfolioId=.*', repl=r'', string=isin_code, flags=0) for isin_code in isin_codes]
 
-            ## Delete objects
-            del element, elements
+        # Import portfolio
+        assets_import_df = (
+            pd.DataFrame(data={'asset_name': asset_names, 'isin_code': isin_codes, 'current_value': current_values}, index=None, dtype=None)
+            # current_value
+            .assign(current_value=lambda row: row['current_value'].replace(to_replace=r'(^.*\u20ac)([0-9]+,[0-9]+\.[0-9]+|[0-9]+\.[0-9]+)(.*)?', value=r'\2', regex=True))
+            .assign(current_value=lambda row: row['current_value'].replace(to_replace=r',', value=r'', regex=False))
+            # .astype(dtype={'current_value': 'float'})
+            .filter(items=['asset_name', 'isin_code', 'shares', 'current_value'])
+        )
 
-            ## Clean 'isin_codes'
-            isin_codes = [re.sub(pattern=r'https://de.scalable.capital/broker/security\?isin=|&portfolioId=.*', repl=r'', string=isin_code, flags=0) for isin_code in isin_codes]
+        # Delete objects
+        del asset_names, current_values
 
-            # Import portfolio
-            assets_import_df = (
-                pd.DataFrame(data={'asset_name': asset_names, 'isin_code': isin_codes, 'current_value': current_values}, index=None, dtype=None)
-                # current_value
-                .assign(current_value=lambda row: row['current_value'].replace(to_replace=r'(^.*\u20ac)([0-9]+,[0-9]+\.[0-9]+|[0-9]+\.[0-9]+)(.*)?', value=r'\2', regex=True))
-                .assign(current_value=lambda row: row['current_value'].replace(to_replace=r',', value=r'', regex=False))
-                # .astype(dtype={'current_value': 'float'})
-                .filter(items=['asset_name', 'isin_code', 'shares', 'current_value'])
-            )
+        # shares
+        shares = []
 
-            # Delete objects
-            del asset_names, current_values
+        for isin_code in isin_codes:
+            driver.get(url=f'https://de.scalable.capital/broker/security?isin={isin_code}&portfolioId={portfolio_id}')
 
-            # shares
-            shares = []
+            # Wait until the element with the text "Shares" is found
+            WebDriverWait(driver=driver, timeout=10).until(method=EC.presence_of_element_located(locator=(By.XPATH, '//div[contains(text(), "Shares")]//..//span')))
 
-            for isin_code in isin_codes:
-                driver.get(url=f'https://de.scalable.capital/broker/security?isin={isin_code}&portfolioId={portfolio_id}')
+            share_value = driver.find_element(by=By.XPATH, value='//div[contains(text(), "Shares")]//..//span').text
+            share_value = re.sub(pattern=r'^\u20ac|,', repl=r'', string=share_value, flags=0)
+            share_value = float(share_value)
+            shares.append({'isin_code': isin_code, 'shares': share_value})
 
-                # Wait until the element with the text "Shares" is found
-                WebDriverWait(driver=driver, timeout=10).until(method=EC.presence_of_element_located(locator=(By.XPATH, '//div[contains(text(), "Shares")]//..//span')))
+        # Create DataFrame
+        shares_df = pd.DataFrame(data=shares, index=None, dtype=None)
 
-                share_value = driver.find_element(by=By.XPATH, value='//div[contains(text(), "Shares")]//..//span').text
-                share_value = re.sub(pattern=r'^\u20ac|,', repl=r'', string=share_value, flags=0)
-                share_value = float(share_value)
-                shares.append({'isin_code': isin_code, 'shares': share_value})
+        # Left join 'assets_import_df' with 'shares_df'
+        assets_import_df = pd.merge(left=assets_import_df, right=shares_df, how='left', on=['isin_code'], indicator=False).filter(items=['asset_name', 'isin_code', 'shares', 'current_value'])
 
-            # Create DataFrame
-            shares_df = pd.DataFrame(data=shares, index=None, dtype=None)
+        # Delete objects
+        del isin_codes, shares, shares_df
 
-            # Left join 'assets_import_df' with 'shares_df'
-            assets_import_df = pd.merge(left=assets_import_df, right=shares_df, how='left', on=['isin_code'], indicator=False).filter(items=['asset_name', 'isin_code', 'shares', 'current_value'])
+        # Metadata
+        assets_import_df = (
+            assets_import_df.assign(date=pd.Timestamp.now().date())
+            .assign(type='Investments')
+            .assign(financial_institution='Scalable Capital')
+            .filter(items=['date', 'type', 'financial_institution', 'asset_name', 'isin_code', 'shares', 'current_value'])
+            .sort_values(by=['date', 'financial_institution', 'isin_code'], ignore_index=True)
+        )
 
-            # Delete objects
-            del isin_codes, shares, shares_df
+        # Concatenate DataFrame
+        assets_df = pd.concat(objs=[assets_df, assets_import_df], axis=0, ignore_index=False, sort=False)
 
-            # Metadata
-            assets_import_df = (
-                assets_import_df.assign(date=pd.Timestamp.now().date())
-                .assign(type='Investments')
-                .assign(financial_institution='Scalable Capital')
-                .filter(items=['date', 'type', 'financial_institution', 'asset_name', 'isin_code', 'shares', 'current_value'])
-                .sort_values(by=['date', 'financial_institution', 'isin_code'], ignore_index=True)
-            )
-
-            # Concatenate DataFrame
-            assets_df = pd.concat(objs=[assets_df, assets_import_df], axis=0, ignore_index=False, sort=False)
-
-            # Delete objects
-            del assets_import_df
+        # Delete objects
+        del assets_import_df
 
     # Save
     if file_type == '.xlsx' and output_path is not None:
